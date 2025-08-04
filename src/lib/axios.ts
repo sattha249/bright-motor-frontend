e474071f416d7e9176370710039e5f995e5126b9
Fisho180k <91049335+sattha249@users.noreply.github.com>
2025-08-04 18:19:42 +0700

    test change path

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useUserStore } from '@/stores/user'; // นำเข้า Pinia Store
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
     withCredentials: false,
 });
 
